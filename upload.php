d1da966df4a41e202e4cab5eb24137b6753dd477
Victor <victorjdacruz@gmail.com>
2020-06-05 15:14:45 -0300

    Correção de bug botão gravar novamente; Correção de bug botão enviar; Código de envio de áudio do gravador para o servidor finalizado;

--- upload.php
+++ upload.php
@@ -1,29 +1,44 @@
 <?php
-print_r($_FILES); //this will print out the received name, temp name, type, size, etc.
-$bairro = $_POST['bairro'];
-$cidade = $_POST['cidade'];
-$latitude = $_POST['latitude'];
-$longitude = $_POST['longitude'];
-$permission = $_POST['permission'];
+//print_r($_FILES); //this will print out the received name, temp name, type, size, etc.
 
-if ($permission === "true"){
-    echo("Latitude: $latitude");
-    echo("Longitude: $longitude");
-}else{
-    echo("Bairro: $bairro");
-    echo("Cidade: $cidade");
+if (isset($_POST['bairro'])){
+    $bairro = $_POST['bairro'];
+}if (isset($_POST['cidade'])){
+    $cidade = $_POST['cidade'];
+}if (isset($_POST['latitude'])){
+    $latitude = $_POST['latitude'];
+}if (isset($_POST['longitude'])){
+    $longitude = $_POST['longitude'];
+}if (isset($_POST['permission'])){
+    $permission = $_POST['permission'];
 }
-
 
 $size = $_FILES['audio_data']['size']; //the size in bytes
 $input = $_FILES['audio_data']['tmp_name']; //temporary name that PHP gave to the uploaded file
-$output = $_FILES['audio_data']['name'].".wav"; //letting the client control the filename is a rather bad idea
+//$output = $_FILES['audio_data']['name'].".wav"; //letting the client control the filename is a rather bad idea
 
 //move the file from temp name to local folder using $output name
-move_uploaded_file($input, $output);
+//move_uploaded_file($input, $output);
+//echo "Áudio: ".$output."\n";
+echo "Permission:".$permission."\n";
+if ($permission === "true"){
+    echo("Latitude: $latitude \n");
+    echo("Longitude: $longitude \n");
+    $loc = "__".$longitude.",".$latitude;
+}else{
+    echo("Bairro: $bairro \n");
+    echo("Cidade: $cidade \n");
+    $loc = "__".$cidade.",".$bairro;
+}
 
-print_r($output);
+// Move o arquivo da pasta temporaria de upload para a pasta de destino 
+$dir = "novos_relatos/"; 
 
-
+if (move_uploaded_file($input, "$dir/".$_FILES['audio_data']['name'].$loc.".wav")) { 
+    echo "Arquivo enviado com sucesso! \n"; 
+} 
+else { 
+    echo "Erro, o arquivo nao enviado! \n"; 
+}  
 
 ?>
