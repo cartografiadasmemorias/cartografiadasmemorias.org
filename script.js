b362d880c2f4f8aacd13a46230ffe94d616e4ec4
Sara Lana <saralgc@gmail.com>
2020-06-24 13:38:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,29 @@ geojson.features.forEach(function(marker) {
     .setLngLat(marker.geometry.coordinates)
     .addTo(map);
 });
+
+function Sobre() {
+  document.getElementById('sobre').style.zIndex = 1;
+  document.getElementById('sobre-nav').style.color = "#b2c9c9";
+  document.getElementById('termos').style.zIndex = -1;
+  document.getElementById('termos-nav').style.color = "#23446c";
+  document.getElementById('contato').style.zIndex = -1;
+  document.getElementById('contato-nav').style.color = "#23446c";
+}
+function Termos() {
+  document.getElementById('sobre').style.zIndex = -1;
+  document.getElementById('sobre-nav').style.color = "#23446c";
+  document.getElementById('termos').style.zIndex = 1;
+  document.getElementById('termos-nav').style.color = "#b2c9c9";
+  document.getElementById('contato').style.zIndex = -1;
+  document.getElementById('contato-nav').style.color = "#23446c";
+}
+function Contato() {
+  document.getElementById('sobre').style.zIndex = -1;
+  document.getElementById('sobre-nav').style.color = "#23446c";
+  document.getElementById('termos').style.zIndex = -1;
+  document.getElementById('termos-nav').style.color = "#23446c";
+  document.getElementById('contato').style.zIndex = 1;
+  document.getElementById('contato-nav').style.color = "#b2c9c9";
+  
+}
