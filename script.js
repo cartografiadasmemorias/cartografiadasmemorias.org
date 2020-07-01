20cc6ce59a87a34cd0f31a6a2fc1f900ea293442
Sara Lana <saralgc@gmail.com>
2020-06-30 23:14:22 -0300

    nomes

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function Sobre() {
   document.getElementById('sobre').style.zIndex = 1;
-  document.getElementById('sobre-nav').style.color = "#b2c9c9";
+  document.getElementById('sobre-nav').style.color = "#23446c";
   document.getElementById('termos').style.zIndex = -1;
-  document.getElementById('termos-nav').style.color = "#23446c";
+  document.getElementById('termos-nav').style.color = "#b2c9c9";
   document.getElementById('contato').style.zIndex = -1;
-  document.getElementById('contato-nav').style.color = "#23446c";
+  document.getElementById('contato-nav').style.color = "#b2c9c9";
 }
 function Termos() {
   document.getElementById('sobre').style.zIndex = -1;
-  document.getElementById('sobre-nav').style.color = "#23446c";
+  document.getElementById('sobre-nav').style.color = "#b2c9c9";
   document.getElementById('termos').style.zIndex = 1;
-  document.getElementById('termos-nav').style.color = "#b2c9c9";
+  document.getElementById('termos-nav').style.color = "#23446c";
   document.getElementById('contato').style.zIndex = -1;
-  document.getElementById('contato-nav').style.color = "#23446c";
+  document.getElementById('contato-nav').style.color = "#b2c9c9";
 }
 function Contato() {
   document.getElementById('sobre').style.zIndex = -1;
-  document.getElementById('sobre-nav').style.color = "#23446c";
+  document.getElementById('sobre-nav').style.color = "#b2c9c9";
   document.getElementById('termos').style.zIndex = -1;
-  document.getElementById('termos-nav').style.color = "#23446c";
+  document.getElementById('termos-nav').style.color = "#b2c9c9";
   document.getElementById('contato').style.zIndex = 1;
-  document.getElementById('contato-nav').style.color = "#b2c9c9";
+  document.getElementById('contato-nav').style.color = "#23446c";
   
 }
