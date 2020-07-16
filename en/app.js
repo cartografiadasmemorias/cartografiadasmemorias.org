6d8bfcad9be81c181c0ea72a75e5726f4c6e9d11
Victor <victorjdacruz@gmail.com>
2020-07-16 12:12:25 -0300

    Correcao bug msg de confirmacao e botoes send e again inseridos

--- en/app.js
+++ en/app.js
@@ -158,7 +158,7 @@ function createDownloadLink(blob) {
 
 	//Regravar áudio
 	var reset = document.createElement('button');
-	reset.innerHTML = "<img src=\"media/gravar_novamente.png\"/ style='width:90%;  display:inline;'>";
+	reset.innerHTML = "<img src=\"media/again.png\"/ style='width:80%;  display:inline;'>";
 	//reset.innerHTML = "Gravar Novamente";
 	reset.id = "reset";
 	reset.addEventListener("click", function (event) {
@@ -182,7 +182,7 @@ function createDownloadLink(blob) {
 	//upload link
 	var upload = document.createElement('button');
 	//upload.href="#";
-	upload.innerHTML = "<img src=\"media/enviar_relato.png\"/  style='width:70%; display:inline'>";
+	upload.innerHTML = "<img src=\"media/send.png\"/  style='width:90%; display:inline'>";
 	upload.id = "upload";
 	upload.addEventListener("click", function (event) {
 		enviar();
